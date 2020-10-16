ee0a5ec76f79fbe0a240918efdbb8b975601a5c6
Alexander Taylor <alexandertaylor@Alexanders-MacBook-Pro-3.local>
2020-10-16 22:33:26 +0100

    Add London mesh

--- src/components/three-scene.vue
+++ src/components/three-scene.vue
@@ -6,7 +6,7 @@
 <script>
 import * as THREE from 'three';
 // eslint-disable-next-line no-unused-vars
-import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // set globals used by Three
@@ -16,18 +16,33 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, windowWidth / windowHeight, 0.01, 10000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const controls = new OrbitControls(camera, renderer.domElement);
+const gltfLoader = new GLTFLoader();
 
 export default {
   name: 'ThreeScene',
   mounted() {
     this.initThreeScene();
+    this.loadMapModel();
+    this.addLighting();
   },
   methods: {
     initThreeScene() {
+      camera.position.set(0, 0, 100);
       renderer.setSize(windowWidth, windowHeight);
       scene.add(camera);
       this.$refs.threeScene.appendChild(renderer.domElement);
       this.animate();
+    },
+    addLighting() {
+      const light = new THREE.AmbientLight(0xffffff, 10);
+      scene.add(light);
+    },
+    loadMapModel() {
+      gltfLoader.load('./assets/glb/london-map.glb', (asset) => {
+        const { scenes } = asset;
+        const [model] = scenes;
+        scene.add(model);
+      });
     },
     animate() {
       const render = () => {
